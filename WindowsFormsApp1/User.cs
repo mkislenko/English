32858aa8f8e8a1278215b450423a07dfd6003c09
mkislenko <mkislenko@gmail.com>
2018-07-03 10:58:48 +0300

    Test

---
+++ WindowsFormsApp1/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class User
+    {
+        public string name;
+
+        public List<string> results;
+
+        public User(string name)
+        {
+
+        }
+    }
+}
